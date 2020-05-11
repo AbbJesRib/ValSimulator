a134ee2274181d5f0c3b6715465850064d43bd52
AbbJesRib <54663096+AbbJesRib@users.noreply.github.com>
2020-05-11 12:40:23 +0200

    Föremål som importeras in i Valsimulator.py

---
+++ assets.py
@@ -0,0 +1,118 @@
+from random import randint
+
+Vänster = bool
+
+gröng = {
+    "namn": "Gröngölingarna",
+    Vänster: True,
+    "Block": "Småpartierna",
+    min: 3,
+    max: 12,
+    "ledare": "Jonas Ostbåge"
+}
+part = {
+    "namn": "Partikelpartiet",
+    Vänster: True,
+    "Block": "Småpartierna",
+    min: 2,
+    max: 8,
+    "ledare": "Hans Majonäs"
+}
+mälar = {
+    "namn": "Mälarpartiet",
+    Vänster: False,
+    "Block": "Småpartierna",
+    min: 8,
+    max: 18,
+    "ledare": "Pernilla Godisgorilla"
+}
+sjö = {
+    "namn": "Sjörövarpartiet",
+    Vänster: False,
+    "Block": "Småpartierna",
+    min: 3,
+    max: 12,
+    "ledare": "Arja Samerna"
+}
+extrem = {
+    "namn": "Extremisterna",
+    Vänster: False,
+    "Block": "Oljeblocket",
+    min: 3,
+    max: 6,
+    "ledare": "Lennart Lurig"
+}
+maskin = {
+    "namn": "Maskinpartiet",
+    Vänster: True,
+    "Block": "Oljeblocket",
+    min: 12,
+    max: 22,
+    "ledare": "Robert Rostbiff"
+}
+framtid = {
+    "namn": "Framtidspartiet",
+    Vänster: False,
+    "Block": "Oljeblocket",
+    min: 12,
+    max: 18,
+    "ledare": "Antwon Släp"
+}
+allp = {
+    "namn": "Allpartiet",
+    Vänster: True,
+    "Block": "Inget",
+    min: 20,
+    max: 34,
+    "ledare": "Dan Dan"
+}
+
+partier = [
+    gröng,
+    part,
+    mälar,
+    sjö,
+    extrem,
+    maskin,
+    framtid,
+    allp
+]
+
+nöjd = [
+    '"Det här gör oss alla i partiet väldigt nöjda. Jag hoppas att vårat nya inflytande i riksdagen kommer gå en lång väg."',
+    '"Det här gick jättebra. Jag lovar att ge alla som röstade på mitt parti 10 000 kr."',
+    '"Tack för att ni röstade på vårt parti."',
+    '"Jäklar! Valfusk funkar ju."',
+    '"Jag ser en bra framtid för Sverige."',
+    '"Jag visste att det här skulle hända."',
+    '"Jarrå, gubbar, så ska det gå."'
+]
+
+missnöjd = [
+    '"Fyfan vad arg jag är"',
+    '"Det svenska folket är äckliga"',
+    '"Det här är en katastrof, inte bara för partiet, men för hela Sverige"',
+    '"Det här måste ju ha varit valfusk. Jag ser ingen annan förklaring"',
+    '"Jaha, tillbaka till ritbordet."',
+    '"Jag visste att det här skulle hända."',
+    '"Det här kunde ingen i partiet ha förväntat sig. Jag skäms."'
+]
+
+adj = [
+    "katastrofala",
+    "oerhört dåliga",
+    "usla",
+    "urusla",
+    "hemska",
+    "förfärliga",
+    "misslyckade"
+]
+
+
+
+def randomrösta(a, b):
+    for i in a:
+        b.append(randint(i[min], i[max]))
+    if sum(b) != 100:
+        b.clear()
+        randomrösta(a, b)
